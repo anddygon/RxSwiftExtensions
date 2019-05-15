353ac440bd5dacfd36a6cf97a752a1c936a57e96
xiaop <gongxiaopeng@apusapps.com>
2019-05-15 12:20:21 +0800

    优化实现

--- Classes/Reactive+ViewController.swift
+++ Classes/Reactive+ViewController.swift
@@ -34,51 +34,61 @@ public extension Reactive where Base: UIViewController {
      it's highly unlikely it would have any benefit in real world use cases, and it's even more
      dangerous.
      */
-    var viewWillAppear: Observable<Void> {
-        return base.rx.methodInvoked(#selector(UIViewController.viewWillAppear(_:)))
-            .mapVoid()
+    var viewDidLoad: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewDidLoad)).mapVoid()
+        return ControlEvent(events: source)
     }
     
-    var viewDidAppear: Observable<Void> {
-        return base.rx.methodInvoked(#selector(UIViewController.viewDidAppear(_:)))
-            .mapVoid()
+    var viewWillAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewWillAppear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
     }
     
-    var viewWillDisappear: Observable<Void> {
-        return base.rx.methodInvoked(#selector(UIViewController.viewWillDisappear(_:)))
-            .mapVoid()
+    var viewDidAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewDidAppear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
     }
     
-    var viewDidDisappear: Observable<Void> {
-        return base.rx.methodInvoked(#selector(UIViewController.viewDidDisappear(_:)))
-            .mapVoid()
+    var viewWillDisappear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewWillDisappear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
     }
     
-    var touchBegan: Observable<(Set<UITouch>, UIEvent?)> {
-        return base.rx.methodInvoked(#selector(UIViewController.touchesBegan(_:with:)))
-            .map({ (params) -> (Set<UITouch>, UIEvent?) in
-                return (params[0] as! Set<UITouch>, params[1] as? UIEvent)
-            })
+    var viewDidDisappear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewDidDisappear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
     }
     
-    var touchMoved: Observable<(Set<UITouch>, UIEvent?)> {
-        return base.rx.methodInvoked(#selector(UIViewController.touchesMoved(_:with:)))
-            .map({ (params) -> (Set<UITouch>, UIEvent?) in
-                return (params[0] as! Set<UITouch>, params[1] as? UIEvent)
-            })
+    var viewWillLayoutSubviews: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewWillLayoutSubviews)).mapVoid()
+        return ControlEvent(events: source)
     }
     
-    var touchEnded: Observable<(Set<UITouch>, UIEvent?)> {
-        return base.rx.methodInvoked(#selector(UIViewController.touchesEnded(_:with:)))
-            .map({ (params) -> (Set<UITouch>, UIEvent?) in
-                return (params[0] as! Set<UITouch>, params[1] as? UIEvent)
-            })
+    var viewDidLayoutSubviews: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewDidLayoutSubviews)).mapVoid()
+        return ControlEvent(events: source)
     }
     
-    var touchCancelled: Observable<(Set<UITouch>, UIEvent?)> {
-        return base.rx.methodInvoked(#selector(UIViewController.touchesCancelled(_:with:)))
-            .map({ (params) -> (Set<UITouch>, UIEvent?) in
-                return (params[0] as! Set<UITouch>, params[1] as? UIEvent)
-            })
+    var willMoveToParentViewController: ControlEvent<UIViewController?> {
+        let source = self.methodInvoked(#selector(Base.willMove)).map { $0.first as? UIViewController }
+        return ControlEvent(events: source)
+    }
+    
+    var didMoveToParentViewController: ControlEvent<UIViewController?> {
+        let source = self.methodInvoked(#selector(Base.didMove)).map { $0.first as? UIViewController }
+        return ControlEvent(events: source)
+    }
+    
+    /// Rx observable, triggered when the ViewController appearance state changes (true if the View is being displayed, false otherwise)
+    var isVisible: Observable<Bool> {
+        let viewDidAppearObservable = self.base.rx.viewDidAppear.map { _ in true }
+        let viewWillDisappearObservable = self.base.rx.viewWillDisappear.map { _ in false }
+        return Observable<Bool>.merge(viewDidAppearObservable, viewWillDisappearObservable)
+    }
+    
+    /// Rx observable, triggered when the ViewController is being dismissed
+    var isDismissing: ControlEvent<Bool> {
+        let source = self.sentMessage(#selector(Base.dismiss)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
     }
 }
